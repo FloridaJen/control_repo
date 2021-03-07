b8c37564b0ac1ce35f5359ea92e25f3cba23407b
Thunderkitten <79342690+JennaMcCarthy@users.noreply.github.com>
2021-03-07 13:38:47 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -13,13 +13,5 @@ node 'master.puppet.vm' {
     owner => 'root',
   }
 }
-node 'minetest.puppet.vm': {
-  include role::minecraft_server
-}
-node /^web/ { 
-  include role::app_server
-}
-node /^db/ {
-  include role::db_server
-}
+
 
